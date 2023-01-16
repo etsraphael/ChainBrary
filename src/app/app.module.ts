a3596c18e7736a9d4e45c768878aa2a9ec5cf081
Nathanael421 <nathanael.rayapin@gmail.com>
2023-01-16 22:11:03 +0100

    Pre-requisite packages (Bootstrap and Angular material)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './page/layout/layout.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
