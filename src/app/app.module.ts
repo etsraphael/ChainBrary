f4182efa3ab08517ea7bb087a6388fcccb46d4a9
Nathanael421 <nathanael.rayapin@gmail.com>
2023-01-24 22:14:16 +0100

    Module file created with all Angular Material modules (Added to app.module.ts) + Prettier Formating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './page/layout/layout.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
