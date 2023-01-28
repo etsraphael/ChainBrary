e35f1ade1eae36603832f98dbd03cfef967f7ea1
etsraphael <ets_raphael@outlook.fr>
2023-01-28 11:18:51 +1000

    CertifiedProfiles updated

--- contracts/ProfileStorage.sol
+++ contracts/ProfileStorage.sol
@@ -1,24 +1,56 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Profile {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CertifiedProfiles is Ownable {
+
     struct ProfileStruct {
+        address userAddress;
         string username;
         string description;
+        uint256 expirationDate;
     }
 
     mapping(address => ProfileStruct) public profiles;
 
+    modifier profileNotNull() {
+        require(profiles[_msgSender()].userAddress != address(0), "Profile not found");
+        _;
+    }
+
+    modifier profileOwner() {
+        require(profiles[_msgSender()].userAddress == _msgSender(), "Not authorized");
+        _;
+    }
+
     function addProfile(string memory _username, string memory _description) public {
-        profiles[msg.sender] = ProfileStruct(_username, _description);
+        require(profiles[_msgSender()].userAddress != _msgSender(), "Profile already exists");
+        uint256 oneYearInSeconds = 365 days;
+        uint256 expirationDate = block.timestamp + oneYearInSeconds;
+        profiles[_msgSender()] = ProfileStruct(_msgSender(), _username, _description, expirationDate);
     }
 
-    function updateProfile(string memory _username, string memory _description) public {
-        profiles[msg.sender].username = _username;
-        profiles[msg.sender].description = _description;
+    function extendExpiredDate() public profileOwner {
+        uint256 oneYearInSeconds = 365 days;
+        uint256 expirationDate = block.timestamp + oneYearInSeconds;
+        profiles[_msgSender()].expirationDate = expirationDate;
     }
 
-    function getProfile(address _address) public view returns (string memory, string memory) {
-        return (profiles[_address].username, profiles[_address].description);
+    function updateProfile(string memory _username, string memory _description) public profileOwner {
+        profiles[_msgSender()].username = _username;
+        profiles[_msgSender()].description = _description;
+    }
+
+    function getProfile(address _address) public view profileNotNull returns (address, string memory, string memory, uint256)  {
+        return (profiles[_address].userAddress, profiles[_address].username, profiles[_address].description, profiles[_address].expirationDate);
+    }
+
+    function deleteProfileByAddress(address _address) public profileNotNull onlyOwner {
+        delete profiles[_address];
+    }
+
+    function deleteMyProfile() public profileOwner {
+        delete profiles[_msgSender()];
     }
 }
