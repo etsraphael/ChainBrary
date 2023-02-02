f598a35b6a3a4b8d13312bb31a29e73c43b53633
rafaelsalei <ets_raphael@outlook.fr>
2023-02-02 21:57:25 +1000

    renaming (#20)

--- UI/src/app/store/global-store/state/selectors.ts
+++ UI/src/app/store/global-store/state/selectors.ts
@@ -1,4 +1,4 @@
 import { createFeatureSelector } from '@ngrx/store';
 import { GLOBAL_FEATURE_KEY, IGlobalState } from './interfaces';
 
-export const selectAuth = createFeatureSelector<IGlobalState>(GLOBAL_FEATURE_KEY);
+export const selectGlobal = createFeatureSelector<IGlobalState>(GLOBAL_FEATURE_KEY);
