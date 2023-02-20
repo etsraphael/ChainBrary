46e3dd2865a77057346cf2e6a48a21f89064d95a
etsraphael <ets_raphael@outlook.fr>
2023-02-21 08:27:55 +1000

    dark mode is working

--- UI/src/app/page/landing-page/components/landing-page-header/landing-page-header.component.ts
+++ UI/src/app/page/landing-page/components/landing-page-header/landing-page-header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ThemeService } from './../../../../shared/services/theme/theme.service';
 
 @Component({
   selector: 'app-landing-page-header',
   templateUrl: './landing-page-header.component.html',
   styleUrls: ['./landing-page-header.component.scss']
 })
-export class LandingPageHeaderComponent {}
+export class LandingPageHeaderComponent {
+  constructor(public themeService: ThemeService) {}
+}
