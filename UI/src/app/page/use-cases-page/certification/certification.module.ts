46f8bc2605c48997cef2f25dbc28c6cb92b9a1e6
etsraphael <ets_raphael@outlook.fr>
2023-03-01 08:43:14 +1000

    avatar building in progress

--- UI/src/app/page/use-cases-page/certification/certification.module.ts
+++ UI/src/app/page/use-cases-page/certification/certification.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CertificationContainerComponent } from './containers/certification-container/certification-container.component';
 import { CertificationEditCardComponent } from './components/certification-edit-card/certification-edit-card.component';
 import { MaterialModule } from './../../../module/material.module';
+import { SharedComponentsModule } from 'src/app/shared/components/shared-components.module';
 
 @NgModule({
   declarations: [CertificationContainerComponent, CertificationEditCardComponent],
-  imports: [CommonModule, MaterialModule]
+  imports: [CommonModule, MaterialModule, SharedComponentsModule]
 })
 export class CertificationModule {}
--- UI/src/app/shared/components/shared-components.module.ts
+++ UI/src/app/shared/components/shared-components.module.ts
@@ -5,10 +5,23 @@ import { FooterPageComponent } from './footer-page/footer-page.component';
 import { HeaderPageComponent } from './header-page/header-page.component';
 import { CertificationMessageComponent } from './certification-message/certification-message.component';
 import { NavBarMobileComponent } from './nav-bar-mobile/nav-bar-mobile.component';
+import { UserAvatarComponent } from './user-avatar/user-avatar.component';
 
 @NgModule({
-  declarations: [FooterPageComponent, HeaderPageComponent, CertificationMessageComponent, NavBarMobileComponent],
+  declarations: [
+    FooterPageComponent,
+    HeaderPageComponent,
+    CertificationMessageComponent,
+    NavBarMobileComponent,
+    UserAvatarComponent
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [FooterPageComponent, HeaderPageComponent, CertificationMessageComponent, NavBarMobileComponent]
+  exports: [
+    FooterPageComponent,
+    HeaderPageComponent,
+    CertificationMessageComponent,
+    NavBarMobileComponent,
+    UserAvatarComponent
+  ]
 })
 export class SharedComponentsModule {}
