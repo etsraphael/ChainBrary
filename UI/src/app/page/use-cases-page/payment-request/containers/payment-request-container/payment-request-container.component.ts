f9d0ddc8c48bee4d80663995f6abf06cef4475e7
etsraphael <ets_raphael@outlook.fr>
2023-03-15 11:44:30 +1000

    PaymentRequestContainerComponent updated

--- UI/src/app/page/use-cases-page/payment-request/containers/payment-request-container/payment-request-container.component.ts
+++ UI/src/app/page/use-cases-page/payment-request/containers/payment-request-container/payment-request-container.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectAuthStatus } from './../../../../../store/auth-store/state/selectors';
+import { AuthStatusCode } from './../../../../../shared/enum';
 
 @Component({
   selector: 'app-payment-request-container',
   templateUrl: './payment-request-container.component.html',
   styleUrls: ['./payment-request-container.component.scss']
 })
-export class PaymentRequestContainerComponent {
+export class PaymentRequestContainerComponent implements OnInit {
   certifficationCardVisible = true;
+  authStatus$: Observable<AuthStatusCode>;
+  AuthStatusCodeTypes = AuthStatusCode;
+
+  constructor(private store: Store) {}
 
   hideCertifficationCard(): void {
     this.certifficationCardVisible = false;
+  }
+
+  ngOnInit(): void {
+    this.authStatus$ = this.store.select(selectAuthStatus);
   }
 }
