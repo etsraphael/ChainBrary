14016a990722eb8ef1274023f5d8168fe20dfb07
etsraphael <ets_raphael@outlook.fr>
2023-03-27 21:13:10 +1000

    environment includes bridge transaction now

--- UI/src/environments/environment.ts
+++ UI/src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   certificationUri: 'https://api.studio.thegraph.com/query/43513/organizationschainbrary/v0.0.4',
   contractLink: {
-    organizationAddress: '0xDbF09e2Fdbb624272AE820585a3619586A66a314'
+    organizationAddress: '0xDbF09e2Fdbb624272AE820585a3619586A66a314',
+    bridgeTransferAddress: '0x32CBCF3fbca572B7fc6Bb2629B5daBC02B671F6B'
   },
   organizationName: 'chainbrary0'
 };
