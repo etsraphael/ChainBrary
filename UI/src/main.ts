1b97fb2038211b443f5e3a96d3e610a01f7bb2f5
etsraphael <ets_raphael@outlook.fr>
2023-05-31 18:10:28 +1000

    Sentry.init only on prod

--- UI/src/main.ts
+++ UI/src/main.ts
@@ -1,25 +1,28 @@
+import { isDevMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import * as Sentry from '@sentry/angular-ivy';
 import { AppModule } from './app/app.module';
 
-Sentry.init({
-  dsn: 'https://415f8b817ed645bebb202692d45953bc@o4504970938023936.ingest.sentry.io/4504970946412544',
-  integrations: [
-    new Sentry.BrowserTracing({
-      tracePropagationTargets: ['https://chainbrary.app.runonflux.io'],
-      routingInstrumentation: Sentry.routingInstrumentation
-    }),
-    new Sentry.Replay({
-      maskAllText: true,
-      blockAllMedia: true
-    })
-  ],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-  tracesSampleRate: 1.0,
-  release: '0.0.3',
-  environment: 'test'
-});
+if (!isDevMode()) {
+  Sentry.init({
+    dsn: 'https://415f8b817ed645bebb202692d45953bc@o4504970938023936.ingest.sentry.io/4504970946412544',
+    integrations: [
+      new Sentry.BrowserTracing({
+        tracePropagationTargets: ['https://chainbrary.app.runonflux.io'],
+        routingInstrumentation: Sentry.routingInstrumentation
+      }),
+      new Sentry.Replay({
+        maskAllText: true,
+        blockAllMedia: true
+      })
+    ],
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+    tracesSampleRate: 1.0,
+    release: '0.0.3',
+    environment: 'test'
+  });
+}
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
