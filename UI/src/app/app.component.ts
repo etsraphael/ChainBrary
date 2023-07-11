e7c19ab60e4386c82cd672135baf68931be82488
etsraphael <ets_raphael@outlook.fr>
2023-07-11 22:01:14 +1000

    getTransactionLogs poc is working

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -29,18 +29,49 @@ export class AppComponent implements OnInit {
     const web3: Web3 = new Web3(window.ethereum);
     const latestBlock: number = await web3.eth.getBlockNumber();
     const fromBlock: number = latestBlock - page * limit;
+    const myAddress = '0xA9ad87470Db27ed18a9a8650f057A7cAab7703Ac';
+    const transferEventSignature: string = web3.utils.sha3('Transfer(address,address,uint256,uint256)') as string;
+    const myAddressPadded: string = web3.utils.padLeft(myAddress, 64);
 
-    web3.eth
-      .getPastLogs({ fromBlock, address: '0xAF19dc1D220774B8D267387Ca2d3E2d452294B81' })
-      .then((res) => {
-        console.log(res);
-        res.forEach((rec) => {
-          web3.eth.getTransaction(rec.transactionHash).then((transaction) => {
-            console.log('Value:', web3.utils.fromWei(transaction.value, 'ether'), 'ETH');
-          });
-        });
-      })
-      .catch((err) => console.log('getPastLogs failed', err));
+    const senderTopics = [transferEventSignature, null, myAddressPadded];
+    const receiverTopics = [transferEventSignature, myAddressPadded, null];
+
+    const [senderLogs, receiverLogs] = await Promise.all([
+      this.getTransactionLogs(web3, fromBlock, senderTopics as string[], 'As Sender'),
+      this.getTransactionLogs(web3, fromBlock, receiverTopics as string[], 'As Receiver')
+    ]);
+
+    const allLogs = [...senderLogs, ...receiverLogs];
+
+    console.log('All Logs:', allLogs);
   }
+
+  async getTransactionLogs(web3: Web3, fromBlock: number, topics: string[], role: string): Promise<any[]> {
+    try {
+      const res = await web3.eth.getPastLogs({
+        fromBlock,
+        address: '0xAF19dc1D220774B8D267387Ca2d3E2d452294B81',
+        topics
+      });
+
+      console.log(role, res);
+
+      const processedLogs = await Promise.all(res.map(async (rec) => {
+        const transaction = await web3.eth.getTransaction(rec.transactionHash);
+        console.log(role, 'Value:', web3.utils.fromWei(transaction.value, 'ether'), 'ETH');
+        return {
+          role,
+          transaction,
+          valueInEther: web3.utils.fromWei(transaction.value, 'ether')
+        };
+      }));
+
+      return processedLogs;
+    } catch (err) {
+      console.log('getPastLogs failed', err);
+      return [];
+    }
+  }
+
 
 }
