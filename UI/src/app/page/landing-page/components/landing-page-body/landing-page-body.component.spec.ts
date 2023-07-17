9afbc7bf588b17ef82f92cbfaabef3b3a107bbeb
Nathanael421 <nathanael.rayapin@gmail.com>
2023-07-17 20:25:52 +0200

    fix: Landing page units testing added

--- UI/src/app/page/landing-page/components/landing-page-body/landing-page-body.component.spec.ts
+++ UI/src/app/page/landing-page/components/landing-page-body/landing-page-body.component.spec.ts
@@ -39,4 +39,39 @@ describe('LandingPageBodyComponent', () => {
       expect(card.icon).toEqual(icons[index]);
     });
   });
+
+  it('should have an array of cards', () => {
+    expect(cards).toBeDefined();
+    expect(Array.isArray(cards)).toBeTruthy();
+  });
+
+  it('should have cards with title property', () => {
+    const titles = ['Open Source', 'Accessible', 'Transparent'];
+
+    cards.forEach((card: LandingPageCard, index: number) => {
+      expect(card.title).toBeDefined();
+      expect(card.title).toBeTypeOf('string');
+      expect(card.title.length).toBeGreaterThan(0);
+      expect(card.title).toEqual(titles[index]);
+    });
+  });
+
+  it('should have cards with description property', () => {
+    cards.forEach((card: LandingPageCard) => {
+      expect(card.description).toBeDefined();
+      expect(card.description).toBeTypeOf('string');
+      expect(card.description.length).toBeGreaterThan(0);
+    });
+  });
+
+  it('should have cards with icon property', () => {
+    const icons = ['bi-file-earmark-code', 'bi-universal-access', 'bi-eye'];
+
+    cards.forEach((card: LandingPageCard, index: number) => {
+      expect(card.icon).toBeDefined();
+      expect(card.icon).toBeTypeOf('string');
+      expect(card.icon.length).toBeGreaterThan(0);
+      expect(card.icon).toEqual(icons[index]);
+    });
+  });
 });
