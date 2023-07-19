8ec4314bf8e96a62e1f20ca68fc88dfe81a1fbe4
etsraphael <ets_raphael@outlook.fr>
2023-07-19 19:14:08 +1000

    price fixed

--- UI/src/app/shared/contracts/priceFeedContract.ts
+++ UI/src/app/shared/contracts/priceFeedContract.ts
@@ -10,7 +10,7 @@ export class PriceFeedContract extends BaseContract {
     [NetworkChainId.SEPOLIA]: [
       {
         pair: TokenPair.EthToUsd,
-            address: '0x694AA1769357215DE4FAC081bf1f309aDC325306'
+        address: '0x694AA1769357215DE4FAC081bf1f309aDC325306'
       },
       {
         pair: TokenPair.BtcToUsd,
