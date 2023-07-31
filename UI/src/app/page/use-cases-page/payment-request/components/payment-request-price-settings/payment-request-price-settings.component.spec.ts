dfb990bc4ed10c96b8edbbaec34d6d73c9d9644a
etsraphael <ets_raphael@outlook.fr>
2023-08-01 08:34:53 +1000

    PaymentRequest test added

--- UI/src/app/page/use-cases-page/payment-request/components/payment-request-price-settings/payment-request-price-settings.component.spec.ts
+++ UI/src/app/page/use-cases-page/payment-request/components/payment-request-price-settings/payment-request-price-settings.component.spec.ts
@@ -7,7 +7,7 @@ import { PriceSettingsForm } from './../../../../../shared/interfaces';
 import { PaymentRequestPriceSettingsComponent } from './payment-request-price-settings.component';
 
 describe('PaymentRequestPriceSettingsComponent', () => {
-  let component: PaymentRequestPriceSettingsComponent;
+  let component: PaymentRequestPriceSettingsComponent = new PaymentRequestPriceSettingsComponent();
   let fixture: ComponentFixture<PaymentRequestPriceSettingsComponent>;
 
   const priceForm: FormGroup<PriceSettingsForm> = new FormGroup<PriceSettingsForm>({
@@ -30,5 +30,42 @@ describe('PaymentRequestPriceSettingsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have priceForm initialized correctly', () => {
+    expect(component.priceForm).toBeInstanceOf(FormGroup);
+  });
+
+  it('should have networkSymbol initialized correctly', () => {
+    component.networkSymbol = 'MockedSymbol';
+    expect(typeof component.networkSymbol).toEqual('string');
+  });
+
+  it('should have usdConversionRate initialized correctly', () => {
+    component.usdConversionRate = 10;
+    expect(typeof component.usdConversionRate).toBe('number');
+  });
+
+  it('should have tokenConversionRate initialized correctly', () => {
+    component.tokenConversionRate = 15;
+    expect(typeof component.tokenConversionRate).toBe('number');
+  });
+
+  it('should emit goToNextPage event', () => {
+    spyOn(component.goToNextPage, 'emit');
+    component.goToNextPage.emit();
+    expect(component.goToNextPage.emit).toHaveBeenCalled();
+  });
+
+  it('should emit goToPreviousPage event', () => {
+    spyOn(component.goToPreviousPage, 'emit');
+    component.goToPreviousPage.emit();
+    expect(component.goToPreviousPage.emit).toHaveBeenCalled();
+  });
+
+  it('should emit swapCurrency event', () => {
+    spyOn(component.swapCurrency, 'emit');
+    component.swapCurrency.emit();
+    expect(component.swapCurrency.emit).toHaveBeenCalled();
   });
 });
