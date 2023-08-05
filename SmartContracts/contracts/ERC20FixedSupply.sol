57b9a3de4173b2173e03a2702fd7cfd58b2fede3
etsraphael <ets_raphael@outlook.fr>
2023-08-05 19:26:02 +1000

    ERC20FixedSupply added

---
+++ SmartContracts/contracts/ERC20FixedSupply.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20FixedSupply is ERC20, Ownable {
+    constructor() ERC20("RandomToken", "RT") {
+        _mint(msg.sender, 1000);
+    }
+}
