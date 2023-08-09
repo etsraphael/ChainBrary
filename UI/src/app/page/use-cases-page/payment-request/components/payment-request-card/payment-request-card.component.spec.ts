9b1b26481fc8a08235a3340abdb9c4d75496c0a9
Nathanael421 <nathanael.rayapin@gmail.com>
2023-08-09 22:49:24 +0200

    feat: Payment request card and some payment request maker added

--- UI/src/app/page/use-cases-page/payment-request/components/payment-request-card/payment-request-card.component.spec.ts
+++ UI/src/app/page/use-cases-page/payment-request/components/payment-request-card/payment-request-card.component.spec.ts
@@ -1,8 +1,10 @@
 import '@angular/compiler';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi, beforeEach } from 'vitest';
 import { PaymentRequestCardComponent } from './payment-request-card.component';
 import { snackbarMock } from 'src/app/shared/tests/modules/modules.mock';
 import { priceFeedServiceMock, walletServiceMock } from 'src/app/shared/tests/services/services.mock';
+import { paymentRequestMock } from 'src/app/shared/tests/variables/payment-request-card';
+import { AuthStatusCode } from 'src/app/shared/enum';
 
 describe('PaymentRequestCardComponent', () => {
   const component: PaymentRequestCardComponent = new PaymentRequestCardComponent(
@@ -11,7 +13,48 @@ describe('PaymentRequestCardComponent', () => {
     priceFeedServiceMock
   );
 
+  beforeEach(() => {
+    component.tokenConversionRate = undefined;
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set tokenConversionRate value if usd data is enabled', () => {
+    component.paymentRequest = paymentRequestMock;
+    const currentPriceNativeToken = 4500;
+    priceFeedServiceMock.getCurrentPriceOfNativeToken = vi.fn()
+      .mockResolvedValue(currentPriceNativeToken);
+
+    component.setUpCurrentPrice()
+
+    setTimeout(() => {
+      const amount = component.paymentRequest.payment.data?.amount;
+      expect(component.tokenConversionRate).toBe(amount as number / currentPriceNativeToken);
+    }, 100);
+  });
+
+  it('should not set tokenConversionRate value if usd data is disabled', () => {
+    component.paymentRequest = paymentRequestMock;
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    component.paymentRequest.payment.data!.usdEnabled = false;
+
+    component.setUpCurrentPrice();
+
+    setTimeout(() => {
+      expect(component.tokenConversionRate).toBeUndefined();
+    }, 100);
+  });
+
+  it.skip('should submit amount successfully', () => {
+    component.authStatus = AuthStatusCode.VerifiedAndConnected;
+    const spySubmitPayment = vi.spyOn(component.submitPayment, 'emit');
+    // Mock this.web3LoginService.currentNetwork$
+    // walletServiceMock.setNetworkOnStoreForTest(ethereumNetworkMock);
+
+    component.submitAmount()
+
+    expect(spySubmitPayment).toHaveBeenCalled();
   });
 });
