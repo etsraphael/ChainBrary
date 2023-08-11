d4a3cacccb26f6248df7494b783a6c7f55447b7d
Nathanael421 <nathanael.rayapin@gmail.com>
2023-08-11 21:47:48 +0200

    feat: Unit tests for payment request price, profile and review added

--- UI/src/app/shared/tests/modules/modules.mock.ts
+++ UI/src/app/shared/tests/modules/modules.mock.ts
@@ -1,6 +1,8 @@
 import { mock } from 'ts-mockito';
 import { Store } from '@ngrx/store';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
 
 export const storeMock = mock(Store);
 export const snackbarMock = mock(MatSnackBar);
+export const dialogMock = mock(MatDialog);
