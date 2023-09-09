619744fb626a45e2cca9dd355e417b79a6d9791e
Nathanael421 <nathanael.rayapin@gmail.com>
2023-09-09 16:25:26 +0200

    fix: Process undefined

--- UI/vitest.config.ts
+++ UI/vitest.config.ts
@@ -1,7 +1,9 @@
 import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  define: {},
+  define: {
+    'import.meta.env': {}
+  },
   test: {
     environment: 'jsdom',
     exclude: [...configDefaults.exclude, 'src/environments/*'],
