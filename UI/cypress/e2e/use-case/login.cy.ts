56ea7aaa5534b3655685e5528d9cacf206da5461
etsraphael <ets_raphael@outlook.fr>
2023-09-12 12:44:31 +1000

    Start login without Metamask is working

--- UI/cypress/e2e/use-case/login.cy.ts
+++ UI/cypress/e2e/use-case/login.cy.ts
@@ -1,9 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Log an User', () => {
-
   it('Start login without Metamask', () => {
-
     // Go to explore solutions page
     cy.visit(`${Cypress.env('baseUrl')}/`);
     cy.get('[data-id="explore-solutions"]').click();
@@ -13,9 +11,9 @@ describe('Log an User', () => {
     cy.get('lib-web3-login lib-body [data-id="wallet-container-btn"]').click();
 
     // Check snackbar message
-    cy.get('mat-snack-bar-container', { timeout: 1000 }).should('be.visible');
-    // TODO: TO FIX
-    // cy.get('mat-snack-bar-container simple-snack-bar .mat-mdc-snack-bar-label', { timeout: 1000 }).should('have.text', 'Non-Ethereum browser detected. You should consider trying MetaMask!');
-
+    cy.get('mat-snack-bar-container').should('be.visible');
+    cy.get('mat-snack-bar-container simple-snack-bar .mat-mdc-snack-bar-label')
+      .should('be.visible')
+      .contains('Non-Ethereum browser detected');
   });
 });
