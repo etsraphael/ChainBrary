a09233b6f51e117ba8b4e02939fbff8c67640519
etsraphael <ets_raphael@outlook.fr>
2023-09-12 20:53:37 +1000

    metamask-stub added

--- UI/cypress/e2e/use-case/login.cy.ts
+++ UI/cypress/e2e/use-case/login.cy.ts
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import { injectMetaMaskStub } from '../../injectors/metamask-stub';
+
 describe('Log an User', () => {
   it('Start login without Metamask', () => {
     // Go to explore solutions page
@@ -21,55 +23,10 @@ describe('Log an User', () => {
 describe('Log an User With MetaMask', () => {
   it('Log an User With MetaMask Started', () => {
     const WALLET_ADDRESS = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
-    const SIGNED_MESSAGE = '...'; // your signed message
-    const eventListeners = {};
-
-    const determineStubResponse = (request) => {
-      switch (request.method) {
-        case 'eth_accounts':
-        case 'eth_requestAccounts':
-          return [WALLET_ADDRESS];
-        case 'personal_sign':
-          return SIGNED_MESSAGE;
-        default:
-          throw Error(`Unknown request: ${request.method}`);
-      }
-    };
+    const SIGNED_MESSAGE = '...';
 
     // Inject MetaMask
-    cy.on('window:before:load', (win) => {
-      win.ethereum = {
-        isMetaMask: true,
-        chainId: '0x1',
-
-        request: async (request: { method: string }) => {
-          return determineStubResponse(request);
-        },
-
-        on: (event, callback) => {
-          if (!eventListeners[event]) {
-            eventListeners[event] = [];
-          }
-          eventListeners[event].push(callback);
-        },
-
-        addListener: (event, callback) => {
-          if (!eventListeners[event]) {
-            eventListeners[event] = [];
-          }
-          eventListeners[event].push(callback);
-        },
-
-        removeListener: (event, callback) => {
-          if (eventListeners[event]) {
-            const index = eventListeners[event].indexOf(callback);
-            if (index > -1) {
-              eventListeners[event].splice(index, 1);
-            }
-          }
-        }
-      };
-    });
+    injectMetaMaskStub(WALLET_ADDRESS, SIGNED_MESSAGE);
 
     // Go to explore solutions page
     cy.visit(`${Cypress.env('baseUrl')}/`);
---
+++ UI/cypress/injectors/metamask-stub.ts
@@ -0,0 +1,60 @@
+type EthereumRequest = {
+  method: string;
+};
+
+type CallbackFunction = (event: string) => void;
+
+type EthereumStub = {
+  isMetaMask: boolean;
+  chainId: string;
+  request: (request: EthereumRequest) => Promise<string[] | string>;
+  on: (event: string, callback: CallbackFunction) => void;
+  addListener: (event: string, callback: CallbackFunction) => void;
+  removeListener: (event: string, callback: CallbackFunction) => void;
+};
+
+export const injectMetaMaskStub = (WALLET_ADDRESS: string, SIGNED_MESSAGE: string) => {
+  const eventListeners: { [event: string]: CallbackFunction[] } = {};
+
+  const determineStubResponse = (request: EthereumRequest): string[] | string => {
+    switch (request.method) {
+      case 'eth_accounts':
+      case 'eth_requestAccounts':
+        return [WALLET_ADDRESS];
+      case 'personal_sign':
+        return SIGNED_MESSAGE;
+      default:
+        throw Error(`Unknown request: ${request.method}`);
+    }
+  };
+
+  cy.on('window:before:load', (win: Window & typeof globalThis & { ethereum?: EthereumStub }) => {
+    win.ethereum = {
+      isMetaMask: true,
+      chainId: '0x1',
+      request: async (request: EthereumRequest) => {
+        return determineStubResponse(request);
+      },
+      on: (event, callback) => {
+        if (!eventListeners[event]) {
+          eventListeners[event] = [];
+        }
+        eventListeners[event].push(callback);
+      },
+      addListener: (event, callback) => {
+        if (!eventListeners[event]) {
+          eventListeners[event] = [];
+        }
+        eventListeners[event].push(callback);
+      },
+      removeListener: (event, callback) => {
+        if (eventListeners[event]) {
+          const index = eventListeners[event].indexOf(callback);
+          if (index > -1) {
+            eventListeners[event].splice(index, 1);
+          }
+        }
+      }
+    };
+  });
+};
