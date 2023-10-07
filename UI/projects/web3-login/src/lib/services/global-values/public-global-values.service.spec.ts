14df344d59077b8a6a31b6ecb8a1c0500f9c113a
etsraphael <ets_raphael@outlook.fr>
2023-10-07 18:08:58 +1100

    unit test fixed

--- UI/projects/web3-login/src/lib/services/global-values/public-global-values.service.spec.ts
+++ UI/projects/web3-login/src/lib/services/global-values/public-global-values.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
 import { PublicGlobalValuesService } from './public-global-values.service';
+import { NetworkServiceWeb3Login } from '../network/network.service';
 
 describe('PublicGlobalValuesService', () => {
   let service: PublicGlobalValuesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [NetworkServiceWeb3Login, { provide: 'config', useValue: {} }]
+    });
     service = TestBed.inject(PublicGlobalValuesService);
   });
 
--- UI/projects/web3-login/src/lib/services/providers/metamask-provider/metamask-provider.service.spec.ts
+++ UI/projects/web3-login/src/lib/services/providers/metamask-provider/metamask-provider.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MetamaskProviderService } from './metamask-provider.service';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('MetamaskProviderService', () => {
   let service: MetamaskProviderService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [MatSnackBarModule],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: 'config', useValue: {} }
+      ]
+    });
     service = TestBed.inject(MetamaskProviderService);
   });
 
