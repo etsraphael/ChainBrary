182c8c89567228c75f89918d7a52dfa91905de10
etsraphael <ets_raphael@outlook.fr>
2023-10-07 13:43:46 +1100

    Create a payment request is clean

--- UI/cypress/e2e/use-case/payment/payment-maker.cy.ts
+++ UI/cypress/e2e/use-case/payment/payment-maker.cy.ts
@@ -3,24 +3,53 @@
 import '@angular/compiler';
 import { NetworkChainId } from '@chainbrary/web3-login';
 
-describe('Create a payment request', () => {
+describe('Create a payment request with wallet', () => {
   beforeEach(() => {
     cy.login('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', '...', NetworkChainId.ETHEREUM);
   });
 
-  it('Start payment', () => {
+  it('Start payment generate a payment with Metamask', () => {
     // Complete profile information
-    cy.get('app-payment-request-profile-settings [data-id="address-input"]').type(
-      '0xbA3Fc0648186a79baEF8DCeE9e055873F432a351',
-      { force: true }
-    );
     cy.get('app-payment-request-profile-settings [data-id="username-input"]').type('My Username');
     cy.get('app-payment-request-profile-settings [data-id="submit-button"]').click();
 
     // Complete description
-    cy.get('app-payment-request-price-settings [data-id="description-input"]').type(
-      'This is my payment request description',
-      { force: true }
-    );
+    cy.get('app-payment-request-price-settings [data-id="description-input"]')
+      .should('be.visible')
+      .should('be.enabled')
+      .focus()
+      .type('This is my payment request description');
+
+    cy.get('app-payment-request-price-settings [data-id="submit-button"]').click();
+  });
+});
+
+describe('Create a payment request without wallet', () => {
+  beforeEach(() => {
+    cy.visit(`${Cypress.env('baseUrl')}/use-cases/payment-request`);
+  });
+  it('Start payment generate a payment', () => {
+
+    // Complete profile information
+    cy.get('app-payment-request-profile-settings [data-id="address-input"]')
+      .should('be.visible')
+      .should('be.enabled')
+      .focus()
+      .type('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266');
+    cy.get('app-payment-request-profile-settings [data-id="username-input"]').type('My Username');
+    cy.get('app-payment-request-profile-settings [data-id="submit-button"]').click();
+
+
+    // Complete description
+    cy.get('app-payment-request-price-settings').contains('A connected wallet is required for conversion rates');
+    cy.get('app-payment-request-price-settings [data-id="description-input"]').should('be.visible');
+    cy.get('app-payment-request-price-settings [data-id="description-input"]').should('be.enabled');
+
+    cy.get('app-payment-request-price-settings [data-id="description-input"]')
+      .should('be.visible')
+      .should('be.enabled')
+      .focus()
+      .type('This is my payment request description');
+    cy.get('app-payment-request-price-settings [data-id="submit-button"]').click();
   });
 });
