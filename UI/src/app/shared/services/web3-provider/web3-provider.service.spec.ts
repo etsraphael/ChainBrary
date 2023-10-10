579e48f946078261bb5621e54b1bcd9953ad2586
rafaelsalei <ets_raphael@outlook.fr>
2023-10-11 07:36:52 +1000

    Feature/140 ganache test (#270)
    * window.ethereum._state.accounts fixed
    
    * save progress
    
    * Web3ProviderService is working
    
    * Web3ProviderService moved
    
    * web3 login improvement

---
+++ UI/src/app/shared/services/web3-provider/web3-provider.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { Web3ProviderService } from './web3-provider.service';
+
+describe('Web3ProviderService', () => {
+  let service: Web3ProviderService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(Web3ProviderService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
--- UI/src/app/store/global-store/state/reducers.ts
+++ UI/src/app/store/global-store/state/reducers.ts
@@ -1,8 +1,17 @@
-import { Action, ActionReducer, createReducer } from '@ngrx/store';
+import { Action, ActionReducer, createReducer, on } from '@ngrx/store';
+import { setAuthPublicAddress } from '../../auth-store/state/actions';
 import { initialState } from './init';
 import { IGlobalState } from './interfaces';
 
-export const globalReducer: ActionReducer<IGlobalState, Action> = createReducer(initialState);
+export const globalReducer: ActionReducer<IGlobalState, Action> = createReducer(
+  initialState,
+  on(setAuthPublicAddress, (state, { wallet }): IGlobalState => {
+    return {
+      ...state,
+      walletConnected: wallet
+    };
+  })
+);
 
 export function reducer(state: IGlobalState = initialState, action: Action): IGlobalState {
   return globalReducer(state, action);
