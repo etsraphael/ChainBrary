7ede5849d9fa41b047707bb5701ca6efbb6f0f38
etsraphael <ets_raphael@outlook.fr>
2023-10-11 15:29:22 +1100

    ganache is running with customised account

---
+++ SmartContracts/start-ganache.js
@@ -0,0 +1,42 @@
+const ganache = require('ganache');
+
+const accounts = [
+  { secretKey: '0x958c7263fbecaafea7c1d81ff08482ef57655dc4f350890dbc3d7ef5784330dd', balance: '0x56BC75E2D63100000' }, // 100 ETH in Wei
+  { secretKey: '0xc80021beb5f346800d62b3ca2e950853d1e024dc395e955e238899cf02ccaed2', balance: '0x56BC75E2D63100000' }, // 100 ETH in Wei
+];
+
+const server = ganache.server({ accounts });
+
+server.listen(8545, async (err, blockchain) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Ganache is running on port 8545');
+  logsAccounts(accounts);
+});
+
+const logsAccounts = async (accounts) => {
+  try {
+    const provider = server.provider;
+
+    // Requesting accounts
+    const accounts = await provider.request({
+      method: "eth_accounts",
+      params: []
+    });
+
+    // Requesting and logging balances
+    for (const account of accounts) {
+      const balanceHex = await provider.request({
+        method: "eth_getBalance",
+        params: [account, "latest"]
+      });
+      const balanceWei = BigInt(balanceHex);
+      const balanceEth = balanceWei / 10n ** 18n; // Convert from Wei to Ether
+      console.log(`Account: ${account}, Balance: ${balanceEth} ETH`);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
