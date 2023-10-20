4444d3ec67bf5541eb6f41cfc5b11593245de399
etsraphael <ets_raphael@outlook.fr>
2023-10-21 09:48:11 +1100

    unit test fix

--- UI/projects/web3-login/src/lib/services/providers/brave-wallet-provider/brave-wallet-provider.service.spec.ts
+++ UI/projects/web3-login/src/lib/services/providers/brave-wallet-provider/brave-wallet-provider.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BraveWalletProviderService } from './brave-wallet-provider.service';
 
 describe('BraveWalletProviderService', () => {
   let service: BraveWalletProviderService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [MatSnackBarModule],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: 'config', useValue: {} }
+      ]
+    });
     service = TestBed.inject(BraveWalletProviderService);
   });
 
