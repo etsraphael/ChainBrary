9c95a9e7fbb4c0c325a809103458088a068f7459
etsraphael <ets_raphael@outlook.fr>
2023-10-25 20:13:37 +1100

    unit test fixed

--- UI/cypress/e2e/use-case/activity.cy.ts
+++ UI/cypress/e2e/use-case/activity.cy.ts
@@ -3,16 +3,17 @@
 import '@angular/compiler';
 import { NetworkChainId } from '@chainbrary/web3-login';
 
-// describe('Go on activity page with a wallet', () => {
-//   beforeEach(() => {
-//     cy.login('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', '...', NetworkChainId.ETHEREUM);
-//   });
+describe('Go on activity page with a wallet', () => {
+  beforeEach(() => {
+    cy.login('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', '...', NetworkChainId.ETHEREUM);
+  });
 
-//   it('Go on activity page started', () => {
-//     // Complete profile information
-//     cy.get('#btn-use-case-bi-clipboard-data-fill').click();
-//   });
-// });
+  it('Go on activity page started', () => {
+    // Complete profile information
+    cy.visit(`${Cypress.env('baseUrl')}/use-cases/activity`);
+    cy.get('app-transaction-activity-table [data-id="login-btn"]').click();
+  });
+});
 
 describe('Go on activity page without a wallet', () => {
   beforeEach(() => {
