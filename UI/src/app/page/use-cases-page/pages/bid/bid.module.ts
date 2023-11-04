f0b99589ef4a33548f3b87e61b5411f71ef67a52
etsraphael <ets_raphael@outlook.fr>
2023-11-04 12:17:05 +1100

    Add form for bid creation with validation

--- UI/src/app/page/use-cases-page/pages/bid/bid.module.ts
+++ UI/src/app/page/use-cases-page/pages/bid/bid.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { UserCasesSharedComponentsModule } from '../../components/user-cases-shared-components.module';
 import { MaterialModule } from './../../../../module/material.module';
 import { SharedComponentsModule } from './../../../../shared/components/shared-components.module';
 import { BidRoutingModule } from './bid-routing.module';
+import { BidCreationComponent } from './components/bid-creation/bid-creation.component';
 import { HomeBidMenuComponent } from './components/home-bid-menu/home-bid-menu.component';
 import { BidContainerComponent } from './containers/bid-container/bid-container.component';
-import { BidCreationComponent } from './components/bid-creation/bid-creation.component';
 
 @NgModule({
   declarations: [BidContainerComponent, HomeBidMenuComponent, BidCreationComponent],
@@ -17,7 +18,8 @@ import { BidCreationComponent } from './components/bid-creation/bid-creation.com
     BidRoutingModule,
     MaterialModule,
     NgxSkeletonLoaderModule,
-    UserCasesSharedComponentsModule
+    UserCasesSharedComponentsModule,
+    ReactiveFormsModule
   ]
 })
 export class BidModule {}
--- UI/src/app/page/use-cases-page/pages/bid/components/bid-creation/bid-creation.component.ts
+++ UI/src/app/page/use-cases-page/pages/bid/components/bid-creation/bid-creation.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-bid-creation',
   templateUrl: './bid-creation.component.html',
   styleUrls: ['./bid-creation.component.scss']
 })
-export class BidCreationComponent {}
+export class BidCreationComponent implements OnInit {
+  mainForm: FormGroup<BidForm>;
+
+  ngOnInit(): void {
+    this.mainForm = new FormGroup({
+      bidName: new FormControl<string | null>(null, [Validators.required]),
+      ownerName: new FormControl<string | null>(null, [Validators.required]),
+      description: new FormControl<string | null>(null, [Validators.required]),
+      duration: new FormControl<number | null>(null, [Validators.required]),
+      photos: new FormControl<ValidPhoto[] | null>(null),
+      termsAndCond: new FormControl<boolean | null>(null, [Validators.requiredTrue])
+    });
+  }
+}
+
+export interface ValidPhoto {
+  url: string;
+  valid: boolean;
+}
+
+export interface BidForm {
+  bidName: FormControl<string | null>;
+  ownerName: FormControl<string | null>;
+  description: FormControl<string | null>;
+  duration: FormControl<number | null>;
+  photos: FormControl<ValidPhoto[] | null>;
+  termsAndCond: FormControl<boolean | null>;
+}
