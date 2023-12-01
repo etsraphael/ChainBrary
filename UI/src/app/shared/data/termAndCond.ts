626398bda752f92ef9ede9b2d65c32dcad26fefe
etsraphael <ets_raphael@outlook.fr>
2023-12-02 08:50:56 +1100

    Update bidTermAndCond with revised Terms and Conditions

--- UI/src/app/shared/data/termAndCond.ts
+++ UI/src/app/shared/data/termAndCond.ts
@@ -1,54 +1,46 @@
 export const bidTermAndCond = `
+# Terms and Conditions for Smart Contract Auction
+
 Before you proceed with bidding, please carefully read the following terms and conditions:
 
 ## 1. Agreement to Terms
-
-By participating in this auction, you are agreeing to be bound by these Terms. All bidders are subject to these terms, and any participation in the auction will signify acceptance.
+By participating in this auction, you agree to be bound by these Terms. All bidders are subject to these terms, and participation signifies acceptance.
 
 ## 2. Eligibility
-
-Participants must be of legal age and capable of entering into legally binding contracts. By submitting a bid, you confirm that you meet these eligibility requirements.
+Participants must be of legal age and capable of entering into legally binding contracts. By bidding, you confirm that you meet these requirements.
 
 ## 3. Auction Process
-
-- The auction commences at the declared start time and will continue until the stipulated end time.
-- Bids must be placed in the increments specified on the bidding page.
-- The highest bid when the auction concludes will be recognized as the winning bid, contingent upon meeting any reserve price set.
+- The auction starts with the smart contract's deployment and continues until the end time.
+- Bids must be in specified increments.
+- The highest bid at the end, meeting any reserve price, is the winning bid.
 
 ## 4. Bid Submissions
-
-- Bids once placed are final and binding. Retraction of bids after submission is not permitted.
-- Any bid placed within the final ten minutes will extend the auction's end time to allow additional bids, ensuring fair opportunity for all interested parties.
-
-## 5. Payments
-
-- The winning bidder is obliged to fulfill the payment within the predefined timeframe post-auction closure.
-- Payment will consist of the bid amount in addition to any pertinent fees.
+- Bids are final and binding. No retraction is permitted.
+- Bids within the final 10 minutes extend the auction by 10 minutes.
+- The higher bid takes precedence after transaction confirmation.
+- Late bids are returned.
+- The highest bidder cannot retract or outbid themselves.
+- New bids automatically refund the previous highest bidder.
 
 ## 6. Transfer of Ownership
-
-Ownership of the item on auction transfers solely upon the full settlement of the winning bid.
+Ownership transfers upon full settlement of the winning bid.
 
 ## 7. Withdrawals
-
-The auction owner reserves the right to withdraw the auction amount only after the auction has officially ended.
+- Withdrawal of the auction amount is permitted post-auction with at least one bidder.
+- A 0.1% withdrawal fee applies.
+- Withdrawals are allowed only after the auction's official end.
 
 ## 8. Role of Chainbrary
-
-Chainbrary does not exert control over the bid smart contract and serves solely as the facilitator for the auction creator's interface. While Chainbrary has no governance over the smart contract, all interactions are recorded on the blockchain, enhancing safety and transparency for all participants.
+Chainbrary facilitates the auction interface but does not control the smart contract. It's not responsible for any errors or bugs.
 
 ## 9. Liability
-
-The auction platform disclaims liability for any complications arising from network downtime, technical glitches, or user input errors during the bidding activity.
+The platform is not liable for issues like network downtime or user input errors.
 
 ## 10. Amendments
-
-We retain the prerogative to modify these terms and conditions without prior notification. Your continued utilization of the auction platform following any adjustments represents your consent to the updated terms.
+Terms may be modified without notice. Continued use implies agreement to new terms.
 
 ## 11. Contact Us
+For inquiries, reach out on [Discord](https://discord.gg/H3hFVuCrXN).
 
-Should you have inquiries regarding these terms, feel free to reach out to us on <a href="https://discord.gg/H3hFVuCrXN" target="_blank">Discord</a> .
-
-By placing a bid, you affirm that you have reviewed, comprehended, and concurred with these Terms and Conditions.
-
+By bidding, you affirm understanding and agreement with these Terms and Conditions.
 `;
