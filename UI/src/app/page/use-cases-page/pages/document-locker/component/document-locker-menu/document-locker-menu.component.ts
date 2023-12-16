2b4be32723d8098e4cbb5d62269657fa315ffc34
etsraphael <ets_raphael@outlook.fr>
2023-12-16 16:56:05 +1100

    Update document-locker-menu component

--- UI/src/app/page/use-cases-page/pages/document-locker/component/document-locker-menu/document-locker-menu.component.ts
+++ UI/src/app/page/use-cases-page/pages/document-locker/component/document-locker-menu/document-locker-menu.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
-
+import { IUseCasesActionCard } from './../../../../../../page/use-cases-page/components/use-cases-action-card/use-cases-action-card.component';
+import { IUseCasesHeader } from './../../../../../../page/use-cases-page/components/use-cases-header/use-cases-header.component';
 @Component({
   selector: 'app-document-locker-menu',
   templateUrl: './document-locker-menu.component.html',
   styleUrls: ['./document-locker-menu.component.scss']
 })
-export class DocumentLockerMenuComponent {}
+export class DocumentLockerMenuComponent {
+  headerPayload: IUseCasesHeader = {
+    title: 'Document Locker',
+    goBackLink: '/use-cases/services',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, nunc nisl aliquet nunc, vitae aliquam nisl nunc sit amet nisl.'
+  };
+
+  useCaseActionCardsPayload: IUseCasesActionCard[] = [
+    {
+      title: 'Lock a document',
+      descritpion:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl eget ultricies aliquam, nunc nisl aliquet nunc, vitae aliquam nisl nunc sit amet nisl',
+      routerLink: '/use-cases/document-locker/creation',
+      buttonText: 'Start creation'
+    }
+  ];
+}
