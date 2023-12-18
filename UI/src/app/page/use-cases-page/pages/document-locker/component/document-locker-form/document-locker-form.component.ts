d21cb56299bd0c62003e085f57452397a0240804
etsraphael <ets_raphael@outlook.fr>
2023-12-18 19:58:28 +1100

    Update document locker form component

--- UI/src/app/page/use-cases-page/pages/document-locker/component/document-locker-form/document-locker-form.component.ts
+++ UI/src/app/page/use-cases-page/pages/document-locker/component/document-locker-form/document-locker-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { INetworkDetail, NetworkChainId, Web3LoginService } from '@chainbrary/web3-login';
 import { environment } from './../../../../../../../environments/environment';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-document-locker-form',
@@ -15,4 +16,35 @@ export class DocumentLockerFormComponent {
   networkList: INetworkDetail[] = this.networkSupported.map((chainId: NetworkChainId) =>
     this.web3LoginService.getNetworkDetailByChainId(chainId)
   );
+
+  mainForm = new FormGroup<DocumentLockingForm>({
+    documentName: new FormControl<string | null>(null, [Validators.required]),
+    ownerName: new FormControl<string | null>(null, [Validators.required]),
+    desc: new FormControl<string | null>(null, [Validators.required]),
+    price: new FormControl<number | null>(null, [Validators.required]),
+    termsAndCond: new FormControl<boolean | null>(null, [Validators.requiredTrue]),
+    networkChainId: new FormControl<NetworkChainId | null>(null, [Validators.required])
+  });
+
+  submitForm(): void {
+    this.mainForm.markAllAsTouched();
+
+    const formValue = this.mainForm.value;
+
+    console.log(formValue);
+
+    if (this.mainForm.invalid) {
+      return;
+    }
+
+    alert('sent');
+  }
+}
+export interface DocumentLockingForm {
+  documentName: FormControl<string | null>;
+  ownerName: FormControl<string | null>;
+  desc: FormControl<string | null>;
+  price: FormControl<number | null>;
+  termsAndCond: FormControl<boolean | null>;
+  networkChainId: FormControl<NetworkChainId | null>;
 }
