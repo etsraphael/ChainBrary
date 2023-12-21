e2be29ee85898b46b55383cb9f3168ca990f6421
etsraphael <ets_raphael@outlook.fr>
2023-12-22 08:58:18 +1100

    Add login functionality for protected document

--- UI/src/app/page/use-cases-page/pages/document-locker/containers/document-locker-found/document-locker-found.component.ts
+++ UI/src/app/page/use-cases-page/pages/document-locker/containers/document-locker-found/document-locker-found.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Web3LoginService } from '@chainbrary/web3-login';
 import { Actions, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { Observable, ReplaySubject, filter, map, takeUntil, withLatestFrom } from 'rxjs';
@@ -11,6 +12,7 @@ import {
   StoreState
 } from './../../../../../../shared/interfaces';
 import { networkChangeSuccess, setAuthPublicAddress } from './../../../../../../store/auth-store/state/actions';
+import { selectIsConnected } from './../../../../../../store/auth-store/state/selectors';
 import { getDocumentLockerByTxn } from './../../../../../../store/document-locker-store/state/actions';
 import {
   selectDocumentLockerCreation,
@@ -32,6 +34,7 @@ export class DocumentLockerFoundComponent implements OnInit, OnDestroy {
   };
 
   constructor(
+    public web3LoginService: Web3LoginService,
     private readonly store: Store,
     private route: ActivatedRoute,
     private actions$: Actions
@@ -42,6 +45,7 @@ export class DocumentLockerFoundComponent implements OnInit, OnDestroy {
   > = this.store.select(selectSearchDocumentLocked);
   private readonly documentLockerCreationStore$: Observable<StoreState<IDocumentLockerCreation | null>> =
     this.store.select(selectDocumentLockerCreation);
+  readonly userIsConnected$: Observable<boolean> = this.store.select(selectIsConnected);
 
   get documentLocked$(): Observable<IDocumentLockerResponse | IDocumentUnlockedResponse | null> {
     return this.documentLockedStore$.pipe(map((s) => s.data));
