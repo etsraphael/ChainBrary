c49344af27e88f2e358c563a883a43d04f287c0b
etsraphael <ets_raphael@outlook.fr>
2024-01-13 13:08:09 +1100

    Add CommunityVault contract and deploy it in Apollo module

--- SmartContracts/contracts/CommunityVault.sol
+++ SmartContracts/contracts/CommunityVault.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
-contract CommunityPool is Ownable, ReentrancyGuard {
+contract CommunityVault is Ownable, ReentrancyGuard {
     mapping(address => uint256) public stackingBalances;
     mapping(address => uint256) public rewardBalancesAdjusted;
 
