1f24632f9675eef1dfa17463d07b4e8a0dd2f040
etsraphael <ets_raphael@outlook.fr>
2024-01-22 10:29:50 +1100

    Remove console import

--- SmartContracts/contracts/CommunityVault.sol
+++ SmartContracts/contracts/CommunityVault.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
-import "hardhat/console.sol";
 
 contract CommunityVault is Ownable, ReentrancyGuard {
     struct User {
