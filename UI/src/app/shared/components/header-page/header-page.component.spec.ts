c36b6d2089f328ea6a630d4c978acd94747bd464
etsraphael <ets_raphael@outlook.fr>
2024-02-03 14:45:56 +1100

    Add TranslationService to header-page.component.spec.ts and translation.service.spec.ts

--- UI/src/app/shared/components/header-page/header-page.component.spec.ts
+++ UI/src/app/shared/components/header-page/header-page.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslationService } from '../../services/translation/translation.service';
 import { SharedComponentsModule } from '../shared-components.module';
 import { HeaderPageComponent } from './header-page.component';
 
@@ -10,7 +11,10 @@ describe('HeaderPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SharedComponentsModule, RouterTestingModule],
-      declarations: [HeaderPageComponent]
+      declarations: [HeaderPageComponent],
+      providers: [
+        TranslationService
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderPageComponent);
--- UI/src/app/shared/services/translation/translation.service.spec.ts
+++ UI/src/app/shared/services/translation/translation.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
-
+import { MAT_SNACK_BAR_DATA, MatSnackBarRef } from '@angular/material/snack-bar';
+import { SharedTestModule } from '../../components/shared-components.module';
 import { TranslationService } from './translation.service';
 
 describe('TranslationService', () => {
   let service: TranslationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [SharedTestModule],
+      providers: [
+        { provide: MatSnackBarRef, useValue: {} },
+        { provide: MAT_SNACK_BAR_DATA, useValue: {} },
+      ]
+    });
     service = TestBed.inject(TranslationService);
   });
 
