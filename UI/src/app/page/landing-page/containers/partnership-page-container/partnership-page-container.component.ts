d2a92a01abb2129215ecfc291aa8ff878e44a546
etsraphael <ets_raphael@outlook.fr>
2024-02-17 12:05:50 +1100

    Refactor partnership page container component

--- UI/src/app/page/landing-page/containers/partnership-page-container/partnership-page-container.component.ts
+++ UI/src/app/page/landing-page/containers/partnership-page-container/partnership-page-container.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ILandingPageCard } from '../../components/landing-page-card/landing-page-card.component';
 
 @Component({
@@ -39,4 +39,46 @@ export class PartnershipPageContainerComponent {
       description: 'Description 6'
     }
   ];
+
+  sectionsAndDescriptions: ISectionAndDescription[] = [
+    {
+      id: 'section1',
+      title: 'Connect with us on Discord',
+      desc: 'Discord is the chat community where we talk about our product all the time.'
+    },
+    {
+      id: 'section2',
+      title: 'Project Introduction',
+      desc: 'Explain about your project and clarify all doubts and have discuss.'
+    },
+    {
+      id: 'section3',
+      title: 'Integration',
+      desc: 'Get the project onboard and processed by us in no time.'
+    }
+  ];
+
+  currentSection: string;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    // Loop through each section to determine which is in view
+    for (const section of this.sectionsAndDescriptions) {
+      const element = document.getElementById(section.id);
+      const position = element?.getBoundingClientRect();
+      const offset = window.innerHeight / 2;
+
+      if (position && position.top <= offset && position.bottom >= offset) {
+        this.currentSection = section.id;
+        break;
+      }
+
+    }
+  }
+}
+
+interface ISectionAndDescription {
+  id: string;
+  title: string;
+  desc: string;
 }
