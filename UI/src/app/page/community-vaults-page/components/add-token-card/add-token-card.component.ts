6cf4e43b04ce9ba212cb2ba753d18043382d52ff
etsraphael <ets_raphael@outlook.fr>
2024-03-09 14:16:53 +1100

    Update add-token-card component to display network currency symbol

--- UI/src/app/page/community-vaults-page/components/add-token-card/add-token-card.component.ts
+++ UI/src/app/page/community-vaults-page/components/add-token-card/add-token-card.component.ts
@@ -2,19 +2,28 @@ import { Component, Input } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import web3 from 'web3';
 import { FullAndShortNumber } from './../../../../shared/interfaces';
+import { INetworkDetail } from '@chainbrary/web3-login';
 
 @Component({
-  selector: 'app-add-token-card[balance]',
+  selector: 'app-add-token-card[balance][network]',
   templateUrl: './add-token-card.component.html',
   styleUrls: ['./add-token-card.component.scss']
 })
 export class AddTokenCardComponent {
   @Input() balance: FullAndShortNumber | null;
+  @Input() network: INetworkDetail;
 
   mainForm = new FormGroup<IAddTokenForm>({
-    amount: new FormControl<number | null>(0, [Validators.required]),
+    amount: new FormControl<number | null>(0, [Validators.required, Validators.min(0.000001)]),
     termsAndCond: new FormControl<boolean | null>(false, [Validators.requiredTrue])
   });
+
+  submitDisabled(): boolean {
+    if (!this.balance) {
+      return true;
+    }
+    return Number(web3.utils.fromWei(String(this.balance.full), 'ether')) === 0;
+  }
 
   setUpMaxAmount(): void {
     if (this.balance) {
@@ -24,7 +33,8 @@ export class AddTokenCardComponent {
   }
 
   submitForm(): void {
-    console.log(this.mainForm.value);
+    this.mainForm.markAllAsTouched();
+    console.log(this.mainForm);
 
     // if (this.mainForm.valid) {
     //   console.log(this.mainForm.value);
--- UI/src/app/page/community-vaults-page/containers/add-token-page-container/add-token-page-container.component.ts
+++ UI/src/app/page/community-vaults-page/containers/add-token-page-container/add-token-page-container.component.ts
@@ -1,23 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { INetworkDetail, Web3LoginService } from '@chainbrary/web3-login';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, ReplaySubject, Subscription, combineLatest, filter, map, take, takeUntil } from 'rxjs';
 import { IHeaderBodyPage } from './../../../../shared/components/header-body-page/header-body-page.component';
 import { FullAndShortNumber } from './../../../../shared/interfaces';
-import { selectBalance } from './../../../../store/auth-store/state/selectors';
+import { selectBalance, selectCurrentNetwork } from './../../../../store/auth-store/state/selectors';
 
 @Component({
   selector: 'app-add-token-page-container',
   templateUrl: './add-token-page-container.component.html',
   styleUrls: ['./add-token-page-container.component.scss']
 })
-export class AddTokenPageContainerComponent {
+export class AddTokenPageContainerComponent implements OnInit, OnDestroy {
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject();
   headerPayload: IHeaderBodyPage = {
     title: `Community Vault`,
     goBackLink: '/community-vaults/list',
     description: null
   };
+  urlNetworkFound: INetworkDetail | null = null;
 
-  constructor(private readonly store: Store) {}
+  constructor(
+    private readonly store: Store,
+    private route: ActivatedRoute,
+    private web3LoginService: Web3LoginService
+  ) {}
 
   readonly userBalance$: Observable<FullAndShortNumber | null> = this.store.select(selectBalance);
+  readonly currentNetwork$: Observable<INetworkDetail | null> = this.store.select(selectCurrentNetwork);
+
+  get isWrongNetwork(): Observable<boolean> {
+    return combineLatest([this.route.params, this.currentNetwork$]).pipe(
+      filter(([params, network]: [Params, INetworkDetail | null]) => !!params && !!network),
+      takeUntil(this.destroyed$),
+      map(([params, network]) => {
+        return params['chainId'] !== network?.chainId;
+      })
+    );
+  }
+
+  ngOnInit(): void {
+    this.getNetworkDetail();
+  }
+
+  getNetworkDetail(): Subscription {
+    return this.route.params
+      .pipe(
+        filter((params: Params) => !!params['chainId']),
+        take(1)
+      )
+      .subscribe(
+        (params: Params) => (this.urlNetworkFound = this.web3LoginService.getNetworkDetailByChainId(params['chainId']))
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
+    this.destroyed$.unsubscribe();
+  }
 }
