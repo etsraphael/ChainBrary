964b7f3178e2395c75e7f0e036a2109646947ee6
etsraphael <ets_raphael@outlook.fr>
2024-03-25 04:04:13 +1100

    Add imports and dependencies to BountyPageContainerComponent.spec.ts

--- UI/src/app/page/landing-page/containers/bounty-page-container/bounty-page-container.component.spec.ts
+++ UI/src/app/page/landing-page/containers/bounty-page-container/bounty-page-container.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SharedComponentsModule } from './../../../../shared/components/shared-components.module';
 import { BountyPageContainerComponent } from './bounty-page-container.component';
+import { MarkdownModule } from 'ngx-markdown';
 
 describe('BountyPageContainerComponent', () => {
   let component: BountyPageContainerComponent;
@@ -8,6 +11,7 @@ describe('BountyPageContainerComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [SharedComponentsModule, RouterTestingModule, BrowserAnimationsModule, MarkdownModule.forChild()],
       declarations: [BountyPageContainerComponent]
     });
     fixture = TestBed.createComponent(BountyPageContainerComponent);
