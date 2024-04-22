5eeb10f41b811f4221ec355550bf366c0372ea86
etsraphael <ets_raphael@outlook.fr>
2024-04-23 08:26:51 +1000

    Update Pay Now page component and template

--- UI/src/app/page/use-cases-page/pages/payment-request/containers/pay-now-page/pay-now-page.component.ts
+++ UI/src/app/page/use-cases-page/pages/payment-request/containers/pay-now-page/pay-now-page.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-pay-now-page',
   templateUrl: './pay-now-page.component.html',
   styleUrls: ['./pay-now-page.component.scss']
 })
-export class PayNowPageComponent {}
+export class PayNowPageComponent {
+  constructor(public location: Location) {}
+
+}
