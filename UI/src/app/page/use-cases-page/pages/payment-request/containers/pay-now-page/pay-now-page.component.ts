e6a6bdebf693ff16396f08d15e244ba31240c4c4
etsraphael <ets_raphael@outlook.fr>
2024-04-27 14:17:52 +1000

    Update Pay Now page component and template with new token selection feature

--- UI/src/app/page/use-cases-page/pages/payment-request/containers/pay-now-page/pay-now-page.component.ts
+++ UI/src/app/page/use-cases-page/pages/payment-request/containers/pay-now-page/pay-now-page.component.ts
@@ -3,10 +3,17 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { IPaymentRequestRaw, StoreState } from './../../../../../../shared/interfaces';
+import { tokenList } from 'src/app/shared/data/tokenList';
+import { IPaymentRequestRaw, IToken, StoreState } from './../../../../../../shared/interfaces';
 import { FormatService } from './../../../../../../shared/services/format/format.service';
 import { decryptRawPaymentRequest } from './../../../../../../store/payment-request-store/state/actions';
 import { selectRawPaymentRequest } from './../../../../../../store/payment-request-store/state/selectors';
+import { NetworkChainId } from '@chainbrary/web3-login';
+
+interface NetworkGroup {
+  name: string;
+  tokens: IToken[];
+}
 
 @Component({
   selector: 'app-pay-now-page',
@@ -20,6 +27,41 @@ export class PayNowPageComponent implements OnInit {
     private readonly store: Store,
     private route: ActivatedRoute
   ) {}
+
+  tokensAvailable: NetworkGroup[] = [
+    {
+      name: 'Ethereum',
+      tokens: tokenList.filter(
+        (token: IToken) =>
+          token.networkSupport.some((network) => network.chainId === NetworkChainId.ETHEREUM) ||
+          token.nativeToChainId === NetworkChainId.ETHEREUM
+      )
+    },
+    {
+      name: 'Binance Smart Chain',
+      tokens: tokenList.filter(
+        (token: IToken) =>
+          token.networkSupport.some((network) => network.chainId === NetworkChainId.BNB) ||
+          token.nativeToChainId === NetworkChainId.BNB
+      )
+    },
+    {
+      name: 'Avalanche',
+      tokens: tokenList.filter(
+        (token: IToken) =>
+          token.networkSupport.some((network) => network.chainId === NetworkChainId.AVALANCHE) ||
+          token.nativeToChainId === NetworkChainId.AVALANCHE
+      )
+    },
+    {
+      name: 'Polygon',
+      tokens: tokenList.filter(
+        (token: IToken) =>
+          token.networkSupport.some((network) => network.chainId === NetworkChainId.POLYGON) ||
+          token.nativeToChainId === NetworkChainId.POLYGON
+      )
+    }
+  ];
 
   get routeId(): string {
     return this.route.snapshot.params['id'];
