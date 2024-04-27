a793647f9a9fd3bd9b51330ce08a2bd8d418f62d
etsraphael <ets_raphael@outlook.fr>
2024-04-27 15:51:18 +1000

    Update Pay Now page component with new token selection feature

--- UI/src/app/page/use-cases-page/pages/payment-request/containers/pay-now-page/pay-now-page.component.ts
+++ UI/src/app/page/use-cases-page/pages/payment-request/containers/pay-now-page/pay-now-page.component.ts
@@ -1,6 +1,8 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { NetworkChainId } from '@chainbrary/web3-login';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { tokenList } from 'src/app/shared/data/tokenList';
@@ -8,11 +10,16 @@ import { IPaymentRequestRaw, IToken, StoreState } from './../../../../../../shar
 import { FormatService } from './../../../../../../shared/services/format/format.service';
 import { decryptRawPaymentRequest } from './../../../../../../store/payment-request-store/state/actions';
 import { selectRawPaymentRequest } from './../../../../../../store/payment-request-store/state/selectors';
-import { NetworkChainId } from '@chainbrary/web3-login';
 
 interface NetworkGroup {
-  name: string;
+  networkName: string;
+  chainId: NetworkChainId;
   tokens: IToken[];
+}
+
+interface ITokenForm {
+  amount: FormControl<number | null>;
+  tokenId: FormControl<string | null>;
 }
 
 @Component({
@@ -21,16 +28,15 @@ interface NetworkGroup {
   styleUrls: ['./pay-now-page.component.scss']
 })
 export class PayNowPageComponent implements OnInit {
-  constructor(
-    public location: Location,
-    public formatService: FormatService,
-    private readonly store: Store,
-    private route: ActivatedRoute
-  ) {}
+  mainForm = new FormGroup<ITokenForm>({
+    amount: new FormControl<number | null>(10, [Validators.required, Validators.min(0)]),
+    tokenId: new FormControl<string | null>(null, [Validators.required])
+  });
 
   tokensAvailable: NetworkGroup[] = [
     {
-      name: 'Ethereum',
+      networkName: 'Ethereum Network',
+      chainId: NetworkChainId.ETHEREUM,
       tokens: tokenList.filter(
         (token: IToken) =>
           token.networkSupport.some((network) => network.chainId === NetworkChainId.ETHEREUM) ||
@@ -38,7 +44,8 @@ export class PayNowPageComponent implements OnInit {
       )
     },
     {
-      name: 'Binance Smart Chain',
+      networkName: 'Binance Smart Chain Network',
+      chainId: NetworkChainId.BNB,
       tokens: tokenList.filter(
         (token: IToken) =>
           token.networkSupport.some((network) => network.chainId === NetworkChainId.BNB) ||
@@ -46,7 +53,8 @@ export class PayNowPageComponent implements OnInit {
       )
     },
     {
-      name: 'Avalanche',
+      networkName: 'Avalanche Network',
+      chainId: NetworkChainId.AVALANCHE,
       tokens: tokenList.filter(
         (token: IToken) =>
           token.networkSupport.some((network) => network.chainId === NetworkChainId.AVALANCHE) ||
@@ -54,7 +62,8 @@ export class PayNowPageComponent implements OnInit {
       )
     },
     {
-      name: 'Polygon',
+      networkName: 'Polygon Network',
+      chainId: NetworkChainId.POLYGON,
       tokens: tokenList.filter(
         (token: IToken) =>
           token.networkSupport.some((network) => network.chainId === NetworkChainId.POLYGON) ||
@@ -62,16 +71,40 @@ export class PayNowPageComponent implements OnInit {
       )
     }
   ];
+  networkSelected: NetworkChainId;
+
+  constructor(
+    public location: Location,
+    public formatService: FormatService,
+    private readonly store: Store,
+    private route: ActivatedRoute
+  ) {}
 
   get routeId(): string {
     return this.route.snapshot.params['id'];
   }
+
+  // get currentTokenSelected$(): IToken | undefined {
+  //   return this.mainForm.get('tokenId');
+  // }
 
   readonly rawRequest$: Observable<StoreState<IPaymentRequestRaw | null>> = this.store.select(selectRawPaymentRequest);
 
   ngOnInit(): void {
     this.callActions();
   }
+
+  networkSaved(val: NetworkChainId): void {
+    this.networkSelected = val;
+  }
+
+  submitForm(): void {
+    this.mainForm.markAllAsTouched();
+    if (this.mainForm.invalid) return;
+
+    console.log(this.mainForm.value);
+  }
+
 
   private callActions(): void {
     this.store.dispatch(decryptRawPaymentRequest({ encodedRequest: this.routeId }));
