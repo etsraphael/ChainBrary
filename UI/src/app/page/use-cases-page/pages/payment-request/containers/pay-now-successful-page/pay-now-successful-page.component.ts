3aa259a7d6c1b6a626696b766c082848bdec2408
etsraphael <ets_raphael@outlook.fr>
2024-05-04 15:06:23 +1000

    Update Pay Now Successful page component and template with new design and styling

--- UI/src/app/page/use-cases-page/pages/payment-request/containers/pay-now-successful-page/pay-now-successful-page.component.ts
+++ UI/src/app/page/use-cases-page/pages/payment-request/containers/pay-now-successful-page/pay-now-successful-page.component.ts
@@ -1,4 +1,10 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { ITransactionCard } from './../../../../../../shared/interfaces';
+import { selectRecentTransactionsByComponent } from './../../../../../../store/transaction-store/state/selectors';
+import { NetworkChainId } from '@chainbrary/web3-login';
 
 @Component({
   selector: 'app-pay-now-successful-page',
@@ -6,5 +12,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./pay-now-successful-page.component.scss']
 })
 export class PayNowSuccessfulPageComponent {
+  constructor(
+    public location: Location,
+    private readonly store: Store
+  ) {}
 
+  readonly transactionCards$: Observable<ITransactionCard[]> = this.store.select(
+    selectRecentTransactionsByComponent('PaymentPageComponent')
+  );
+
+  // TODO: Remove this test data
+  cardContentTest: ITransactionCard = {
+    title: 'Transaction successful!',
+    type: 'success',
+    hash: '0f9g34jhgf083hgfjf9jk2940fg8h24f9-pgh2',
+    component: 'PayNowSuccessfulPageComponent',
+    chainId: NetworkChainId.ETHEREUM
+  };
 }
