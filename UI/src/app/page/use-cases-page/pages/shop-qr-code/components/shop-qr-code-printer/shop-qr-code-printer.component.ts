d935544a2d4171f28550d8e48b594f10cd2b45a8
etsraphael <ets_raphael@outlook.fr>
2024-05-05 21:48:40 +1000

    feat: Add SVG to PDF conversion for printing QR code in Shop QR Code Printer component

--- UI/src/app/page/use-cases-page/pages/shop-qr-code/components/shop-qr-code-printer/shop-qr-code-printer.component.ts
+++ UI/src/app/page/use-cases-page/pages/shop-qr-code/components/shop-qr-code-printer/shop-qr-code-printer.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { CommonButtonText } from './../../../../../../shared/enum';
+import { jsPDF } from 'jspdf';
+import 'svg2pdf.js';
 
 interface QRCodeForm {
   name: FormControl<string | null>;
@@ -27,6 +29,36 @@ export class ShopQrCodePrinterComponent {
   }
 
   print(): void {
-    return window.print();
+    const doc = new jsPDF({
+      orientation: 'portrait',
+      unit: 'mm',
+      format: 'a4'
+    });
+
+    const svgElement = document.getElementById('shop-qr-code-visual-0') as HTMLElement;
+
+    // Calculate the scale to fit the SVG in A4
+    const pdfWidth = 210;  // A4 width in mm
+    const pdfHeight = 297; // A4 height in mm
+    const svgWidth = svgElement.clientWidth;
+    const svgHeight = svgElement.clientHeight;
+
+    // Scale to fit either width or height
+    const scaleX = pdfWidth / svgWidth;
+    const scaleY = pdfHeight / svgHeight;
+    const scale = Math.min(scaleX, scaleY);
+
+    // Convert SVG to PDF using svg2pdf
+    doc.svg(svgElement, {
+      x: 0,
+      y: 0,
+      width: svgWidth * scale,
+      height: svgHeight * scale,
+    }).then(() => {
+      // Save the PDF
+      doc.save('chainbrary-qr-code.pdf');
+    }).catch(err => {
+      console.error('Error converting SVG to PDF: ', err);
+    });
   }
 }
