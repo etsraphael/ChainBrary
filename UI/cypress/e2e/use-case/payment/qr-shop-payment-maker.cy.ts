dd0e2b8d903547973f94b71fe1d658bd53fcd30a
etsraphael <ets_raphael@outlook.fr>
2024-06-01 17:20:22 +1000

    fix: Fix printing issue in Shop QR Code Printer component

--- UI/cypress/e2e/use-case/payment/qr-shop-payment-maker.cy.ts
+++ UI/cypress/e2e/use-case/payment/qr-shop-payment-maker.cy.ts
@@ -10,7 +10,40 @@ describe('Create a QR code payment for shops', () => {
     cy.get('[data-id="btn-print-qr-code"]').click();
   });
 
-  it('Start payment generate a payment with Metamask', () => {
-    // TODO: Implement test
+  it('Print a QR Code', () => {
+    cy.get('[data-id="business-name"]').type('Test Business');
+    cy.get('[data-id="owner-address"]').type('0xd174c9C31ddA6FFC5E1335664374c1EbBE2144af');
+    cy.get(`#shop-qr-code-visual-0`).click();
+
+    cy.window().then((win) => {
+      cy.stub(win, 'print').as('printStub');
+    });
+
+    // TODO: Fix this
+    // Create a custom stub for window.open
+    // let printWindow: Window;
+    // cy.window().then((win: Cypress.AUTWindow) => {
+    //   const originalOpen = win.open;
+    //   cy.stub(win, 'open').callsFake((url, target, features) => {
+    //     printWindow = originalOpen.call(win, url, target, features);
+    //     cy.stub(printWindow.document, 'write').as('docWriteStub');
+    //     return printWindow;
+    //   }).as('openSpy');
+    // });
+
+    // // Trigger the print action
+    // cy.get('[data-id="btn-print-qr-code"]').click();
+
+    // // Assert that a new window was opened
+    // cy.get('@openSpy').should('have.been.calledOnce');
+
+    // // Ensure printWindow is defined
+    // cy.window().then(() => {
+    //   expect(printWindow).to.not.be.undefined;
+
+    //   // Assert that the document.write method was called
+    //   cy.get('@docWriteStub').should('have.been.calledOnce');
+
+    // });
   });
 });
