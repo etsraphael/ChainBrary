3bc9a33862fe817171e64c1720843e1d17d1cac3
etsraphael <ets_raphael@outlook.fr>
2024-06-03 07:43:09 +1000

    fix: Show error message when required fields are not filled in QR code payment creation

--- UI/cypress/e2e/use-case/payment/qr-shop-payment-maker.cy.ts
+++ UI/cypress/e2e/use-case/payment/qr-shop-payment-maker.cy.ts
@@ -10,40 +10,29 @@ describe('Create a QR code payment for shops', () => {
     cy.get('[data-id="btn-print-qr-code"]').click();
   });
 
+  it.only('Show error message', () => {
+    cy.get('[data-id="business-name"]').type('Test Business');
+    cy.get('[data-id="btn-print-qr-code"]').click();
+
+    cy.get('mat-snack-bar-container simple-snack-bar .mat-mdc-snack-bar-label')
+      .should('be.visible')
+      .contains('Please fill in all the required fields');
+
+    cy.get('[data-id="owner-address"]').type('0xd174c9C31ddA6FFC5E1335664374c1EbBE2144af');
+
+    cy.get('[data-id="btn-print-qr-code"]').click();
+
+    cy.get('mat-snack-bar-container simple-snack-bar .mat-mdc-snack-bar-label')
+      .should('be.visible')
+      .contains('Please select a template first');
+  });
+
   it('Print a QR Code', () => {
     cy.get('[data-id="business-name"]').type('Test Business');
     cy.get('[data-id="owner-address"]').type('0xd174c9C31ddA6FFC5E1335664374c1EbBE2144af');
     cy.get(`#shop-qr-code-visual-0`).click();
 
-    cy.window().then((win) => {
-      cy.stub(win, 'print').as('printStub');
-    });
-
-    // TODO: Fix this
-    // Create a custom stub for window.open
-    // let printWindow: Window;
-    // cy.window().then((win: Cypress.AUTWindow) => {
-    //   const originalOpen = win.open;
-    //   cy.stub(win, 'open').callsFake((url, target, features) => {
-    //     printWindow = originalOpen.call(win, url, target, features);
-    //     cy.stub(printWindow.document, 'write').as('docWriteStub');
-    //     return printWindow;
-    //   }).as('openSpy');
-    // });
-
-    // // Trigger the print action
-    // cy.get('[data-id="btn-print-qr-code"]').click();
-
-    // // Assert that a new window was opened
-    // cy.get('@openSpy').should('have.been.calledOnce');
-
-    // // Ensure printWindow is defined
-    // cy.window().then(() => {
-    //   expect(printWindow).to.not.be.undefined;
-
-    //   // Assert that the document.write method was called
-    //   cy.get('@docWriteStub').should('have.been.calledOnce');
-
-    // });
+    // Check if the name is displayed
+    cy.get('#shop-qr-code-visual-0 text').contains('Test Business').should('exist');
   });
 });
