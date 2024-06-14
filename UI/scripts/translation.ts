0afd68ef14316418f1c70e0d2d6186fa06543242
etsraphael <ets_raphael@outlook.fr>
2024-06-15 09:27:41 +1000

    feat: Update markdown rendering in terms-and-cond-page-container.component.html
    Update the markdown rendering in terms-and-cond-page-container.component.html to include the ngPreserveWhitespaces attribute. This change ensures that whitespaces are preserved when rendering the markdown content, improving the readability of the terms and conditions page.

--- UI/scripts/translation.ts
+++ UI/scripts/translation.ts
@@ -16,7 +16,7 @@ const languageList: KeyAndValue[] = [
   }
 ];
 
-const translateText = async (text: string, targetLanguage: string): Promise<string> => {
+const translateText = async (text: string, targetLanguage: string, desc?: string): Promise<string> => {
   try {
     const response = await openai.chat.completions.create({
       model: 'gpt-4-turbo',
@@ -24,7 +24,7 @@ const translateText = async (text: string, targetLanguage: string): Promise<stri
         { role: 'system', content: 'You are a translation model for a blockchain app.' },
         {
           role: 'user',
-          content: `Translate this English text to ${targetLanguage}. Preserve and include any XML tags or placeholders exactly as they are in the translated text without any additional phrases or introductions: ${text}`
+          content: `Translate this English text to ${targetLanguage}. ${desc}. Preserve and include any XML tags or placeholders exactly as they are in the translated text without any additional phrases or introductions.:${text}`
         }
       ]
     });
@@ -40,13 +40,15 @@ const updateTranslations = async () => {
 
     // Match all the translation units that need to be translated
     const matches = [
-      ...originalContent.matchAll(/<target\s+state="new">(.*?)<\/target>/g)
+      ...originalContent.matchAll(
+        /<target\s+(?:state="new")?>(.*?)<\/target>(?:\s*<note\s+priority="1"\s+from="description">(.*?)<\/note>)?/g
+      )
     ];
 
     // Translate all matches concurrently
     const translations = await Promise.all(
-      matches.map(async ([match, originalText]) => {
-        const newtranslation = await translateText(originalText, language.value);
+      matches.map(async ([match, originalText, desc]) => {
+        const newtranslation = await translateText(originalText, language.value, desc);
         return match.replace(`<target state="new">${originalText}</target>`, `<target>${newtranslation}</target>`);
       })
     );
