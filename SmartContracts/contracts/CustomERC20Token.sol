dbcf7f6f4f16ead9290dbb870c798853617c7a19
etsraphael <ets_raphael@outlook.fr>
2024-07-03 07:57:10 +1000

    refactor: Add burn functionality to ERC20Token contract
    The ERC20Token contract has been updated to include burn functionality. This allows token holders to burn their tokens, reducing the total supply. The burn function has been added to the contract, along with the necessary modifiers to ensure that only burnable tokens can be burned. This change improves the flexibility and utility of the ERC20Token contract.

--- SmartContracts/contracts/CustomERC20Token.sol
+++ SmartContracts/contracts/CustomERC20Token.sol
@@ -5,9 +5,12 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
 
-contract ERC20Token is Initializable, ERC20Upgradeable, ERC20PermitUpgradeable, OwnableUpgradeable {
+
+contract ERC20Token is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20PermitUpgradeable, OwnableUpgradeable {
     bool private _isMintable;
+    bool private _isBurnable;
 
     constructor() {
         _disableInitializers();
@@ -17,16 +20,23 @@ contract ERC20Token is Initializable, ERC20Upgradeable, ERC20PermitUpgradeable, 
         address initialOwner,
         string memory name,
         string memory symbol,
-        bool mintable
+        bool mintable,
+        bool burnable
     ) public initializer {
         __ERC20_init(name, symbol);
         __ERC20Permit_init(name);
         __Ownable_init(initialOwner);
         _isMintable = mintable;
+        _isBurnable = burnable;
     }
 
     modifier onlyIfMintable() {
         require(_isMintable, "Token is not mintable");
+        _;
+    }
+
+    modifier onlyIfBurnable() {
+        require(_isBurnable, "Token is not burnable");
         _;
     }
 
@@ -37,22 +47,35 @@ contract ERC20Token is Initializable, ERC20Upgradeable, ERC20PermitUpgradeable, 
     function isMintable() public view returns (bool) {
         return _isMintable;
     }
+
+    function burn(uint256 amount) public override onlyIfBurnable {
+        super.burn(amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public override onlyIfBurnable {
+        super.burnFrom(account, amount);
+    }
+
+    function isBurnable() public view returns (bool) {
+        return _isBurnable;
+    }
 }
 
 contract ERC20TokenFactory is OwnableUpgradeable {
-    event TokenCreated(address indexed tokenAddress, address indexed owner, bool mintable);
+    event TokenCreated(address indexed tokenAddress, address indexed owner, bool mintable, bool burnable);
 
     function createToken(
         address initialAuthority,
         string memory name,
         string memory symbol,
-        bool mintable
+        bool mintable,
+        bool burnable
     ) external returns (address) {
         ERC20Token newToken = new ERC20Token();
-        newToken.initialize(initialAuthority, name, symbol, mintable);
+        newToken.initialize(initialAuthority, name, symbol, mintable, burnable);
         newToken.transferOwnership(_msgSender());
 
-        emit TokenCreated(address(newToken), _msgSender(), mintable);
+        emit TokenCreated(address(newToken), _msgSender(), mintable, burnable);
         return address(newToken);
     }
 }
