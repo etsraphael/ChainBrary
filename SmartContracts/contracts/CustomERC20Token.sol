876bf106c563ea3003e602a4af20dfb71bee42e2
etsraphael <ets_raphael@outlook.fr>
2024-07-04 07:41:06 +1000

    refactor: Add burn functionality to ERC20Token contract

--- SmartContracts/contracts/CustomERC20Token.sol
+++ SmartContracts/contracts/CustomERC20Token.sol
@@ -1,33 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract ERC20Token is ERC20, ERC20Burnable, Ownable {
+    bool _isMintable;
+    bool _isBurnable;
 
-contract ERC20Token is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20PermitUpgradeable, OwnableUpgradeable {
-    bool private _isMintable;
-    bool private _isBurnable;
-
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize(
+    constructor(
         address initialOwner,
         string memory name,
         string memory symbol,
+        uint256 initialSupply,
         bool mintable,
         bool burnable
-    ) public initializer {
-        __ERC20_init(name, symbol);
-        __ERC20Permit_init(name);
-        __Ownable_init(initialOwner);
+    ) ERC20(name, symbol) Ownable(initialOwner) {
         _isMintable = mintable;
         _isBurnable = burnable;
+        _mint(initialOwner, initialSupply * (10 ** decimals()));
     }
 
     modifier onlyIfMintable() {
@@ -40,7 +32,7 @@ contract ERC20Token is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable
         _;
     }
 
-    function mint(address to, uint256 amount) public onlyIfMintable onlyOwner {
+    function mint(address to, uint256 amount) public onlyIfMintable {
         _mint(to, amount);
     }
 
@@ -58,24 +50,5 @@ contract ERC20Token is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable
 
     function isBurnable() public view returns (bool) {
         return _isBurnable;
-    }
-}
-
-contract ERC20TokenFactory is OwnableUpgradeable {
-    event TokenCreated(address indexed tokenAddress, address indexed owner, bool mintable, bool burnable);
-
-    function createToken(
-        address initialAuthority,
-        string memory name,
-        string memory symbol,
-        bool mintable,
-        bool burnable
-    ) external returns (address) {
-        ERC20Token newToken = new ERC20Token();
-        newToken.initialize(initialAuthority, name, symbol, mintable, burnable);
-        newToken.transferOwnership(_msgSender());
-
-        emit TokenCreated(address(newToken), _msgSender(), mintable, burnable);
-        return address(newToken);
     }
 }
