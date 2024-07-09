ce381be2739c172395154b9ad191a95d134e93f4
etsraphael <ets_raphael@outlook.fr>
2024-07-09 10:40:06 +1000

    feat: Add createTokenWithoutConnection$ effect to handle token creation without connection
    This commit adds a new effect, createTokenWithoutConnection$, to the TokenManagementEffects class. The effect listens for the createToken action and checks the authentication status using the selectAuthStatus selector. If the user is not connected, the effect opens the login modal using the web3LoginService. This enhancement improves the user experience by allowing token creation even when the user is not connected.

--- UI/src/app/store/tokens-management-store/state/effects.ts
+++ UI/src/app/store/tokens-management-store/state/effects.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Web3LoginService } from '@chainbrary/web3-login';
-import { Actions } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { concatLatestFrom } from '@ngrx/operators';
 import { Store } from '@ngrx/store';
+import { filter, tap } from 'rxjs';
+import { AuthStatusCode } from 'src/app/shared/enum';
+import { selectAuthStatus } from '../../auth-store/state/selectors';
+import { createToken } from './actions';
 
 @Injectable()
 export class TokenManagementEffects {
@@ -12,4 +17,16 @@ export class TokenManagementEffects {
     private readonly store: Store,
     private router: Router
   ) {}
+
+  createTokenWithoutConnection$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(createToken),
+        concatLatestFrom(() => [this.store.select(selectAuthStatus)]),
+        filter(([, authStatus]) => authStatus === AuthStatusCode.NotConnected),
+        tap(() => this.web3LoginService.openLoginModal())
+      );
+    },
+    { dispatch: false }
+  );
 }
