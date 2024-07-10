be3aa7cecca236baa33fc626bfabd0997b2ca542
etsraphael <ets_raphael@outlook.fr>
2024-07-11 07:42:09 +1000

    feat: Add token creation review component

--- UI/src/app/page/use-cases-page/pages/setup-token/components/token-creation-review/token-creation-review.component.ts
+++ UI/src/app/page/use-cases-page/pages/setup-token/components/token-creation-review/token-creation-review.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ITokenCreationPayload } from './../../../../../../shared/interfaces';
+import { FormatService } from './../../../../../../shared/services/format/format.service';
+import { Web3LoginService } from '@chainbrary/web3-login';
 
 @Component({
-  selector: 'app-token-creation-review',
+  selector: 'app-token-creation-review[tokenPayloadReview]',
   templateUrl: './token-creation-review.component.html',
   styleUrl: './token-creation-review.component.scss'
 })
-export class TokenCreationReviewComponent {}
+export class TokenCreationReviewComponent {
+  @Input() tokenPayloadReview: ITokenCreationPayload;
+  constructor(
+    public formatService: FormatService,
+    public web3LoginService: Web3LoginService
+  ) {}
+}
