913d85a4d12a567d38a7fe752f349cc327b31126
etsraphael <ets_raphael@outlook.fr>
2024-10-05 12:42:34 +1000

    Chore: Add support for Binance Smart Chain and Ethereum Mainnet in constants and token pairs

--- SmartContracts/scripts/constants.ts
+++ SmartContracts/scripts/constants.ts
@@ -9,6 +9,16 @@ export const NETWORKS = {
     chainId: 137,
     rpcUrl: process.env.POLY_MAINNET_URL as string,
     name: 'Polygon Mainnet'
+  },
+  BSC_MAINNET: {
+    chainId: 56,
+    rpcUrl: process.env.BSC_MAINNET_URL as string,
+    name: 'Binance Smart Chain Mainnet'
+  },
+  ETH_MAINNET: {
+    chainId: 1,
+    rpcUrl: process.env.ETH_MAINNET_URL as string,
+    name: 'Ethereum Mainnet'
   }
 };
 
@@ -18,7 +28,10 @@ export const TOKENS = {
     USDC: new Token(137, '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174', 6, 'USDC', 'USD Coin'),
     DAI: new Token(137, '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063', 18, 'DAI', 'Dai Stablecoin'),
     WETH: new Token(137, '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', 18, 'WETH', 'Wrapped Ether')
-    // Add more tokens as needed
+  },
+  ETH: {
+    DAI: new Token(1, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'Dai Stablecoin'),
+    USDC: new Token(1, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 6, 'USDC', 'USD Coin'),
   }
 };
 
@@ -28,7 +41,7 @@ export const TOKEN_PAIRS = [
     network: NETWORKS.POLYGON_MAINNET,
     tokenIn: TOKENS.POLYGON.USDC,
     tokenOut: TOKENS.POLYGON.DAI,
-    amountIn: '100',
+    amountIn: '1',
     fee: 3000
   },
   {
@@ -37,5 +50,13 @@ export const TOKEN_PAIRS = [
     tokenOut: TOKENS.POLYGON.USDC,
     amountIn: '1',
     fee: 3000
+  },
+  {
+    network: NETWORKS.ETH_MAINNET,
+    tokenIn: TOKENS.ETH.USDC,
+    tokenOut: TOKENS.ETH.DAI,
+    amountIn: '1',
+    fee: 3000
   }
+
 ];
