82685607e76e4e67e497f90573e2f7bfdef75d7d
etsraphael <ets_raphael@outlook.fr>
2024-10-31 08:13:07 +1100

    feat(dex): update network selection handling in Dex swapping page with dynamic network details

--- UI/src/app/page/use-cases-page/pages/dex/containers/dex-swapping-page/dex-swapping-page.component.ts
+++ UI/src/app/page/use-cases-page/pages/dex/containers/dex-swapping-page/dex-swapping-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { NetworkDialogComponent } from './../../../../../../shared/components/modal/network-dialog/network-dialog.component';
-import { NetworkChainId } from '@chainbrary/web3-login';
+import { INetworkDetail, NetworkChainId, Web3LoginService } from '@chainbrary/web3-login';
 
 @Component({
   selector: 'app-dex-swapping-page',
@@ -9,24 +9,35 @@ import { NetworkChainId } from '@chainbrary/web3-login';
   styleUrls: ['./dex-swapping-page.component.scss']
 })
 export class DexSwappingPageComponent {
-  constructor(private dialog: MatDialog) {}
-  networkPath: NetworkChainId[] = [NetworkChainId.ETHEREUM, NetworkChainId.ETHEREUM];
+  networkPath: INetworkDetail[] = [
+    this.web3loginService.getNetworkDetailByChainId(NetworkChainId.ETHEREUM),
+    this.web3loginService.getNetworkDetailByChainId(NetworkChainId.ETHEREUM)
+  ];
 
-  openNetowkDialog(): MatDialogRef<NetworkDialogComponent> {
+  constructor(
+    private dialog: MatDialog,
+    private web3loginService: Web3LoginService
+  ) {}
+
+  openNetowkDialog(from: boolean): MatDialogRef<NetworkDialogComponent> {
     const dialogRef: MatDialogRef<NetworkDialogComponent> = this.dialog.open(NetworkDialogComponent, {
       panelClass: ['col-12', 'col-md-8', 'col-lg-6', 'col-xl-5'],
       autoFocus: false
     });
 
     dialogRef.afterClosed().subscribe((chainId: NetworkChainId) => {
-      if (chainId) this.handleNetworkSelected(chainId);
+      if (chainId) this.handleNetworkSelected(chainId, from);
     });
 
     return dialogRef;
   }
 
-  private handleNetworkSelected(chainId: NetworkChainId): void {
+  private handleNetworkSelected(chainId: NetworkChainId, from: boolean): void {
     // Handle the selected network here
-    console.log('Selected Network Chain ID:', chainId);
+    if (from) {
+      this.networkPath[0] = this.web3loginService.getNetworkDetailByChainId(chainId);
+    } else {
+      this.networkPath[1] = this.web3loginService.getNetworkDetailByChainId(chainId);
+    }
   }
 }
