2d4f4016668c3aa235e825ce967c1e2fc7423346
etsraphael <ets_raphael@outlook.fr>
2024-12-16 19:22:41 +1100

    feat(hardhat): add multiple accounts with initial balances for testing

--- SmartContracts/hardhat.config.ts
+++ SmartContracts/hardhat.config.ts
@@ -9,7 +9,53 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       chainId: 31337,
-      loggingEnabled: true
+      loggingEnabled: true,
+      accounts: [
+        {
+          privateKey: "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+          balance: "1000000000000000000000000", // 1000000 ETH
+        },
+        {
+          privateKey: "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
+          balance: "1000000000000000000000000", // 1000000 ETH
+        },
+        {
+          privateKey: "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
+          balance: "1000000000000000000000000", // 1000000 ETH
+        },
+        {
+          privateKey: "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
+          balance: "1000000000000000000000000", // 1000000 ETH
+        },
+        {
+          privateKey: "0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a",
+          balance: "1000000000000000000000000", // 1000000 ETH
+        },
+        {
+          privateKey: "0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba",
+          balance: "1000000000000000000000000", // 1000000 ETH
+        },
+        {
+          privateKey: "0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e",
+          balance: "1000000000000000000000000", // 1000000 ETH
+        },
+        {
+          privateKey: "0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356",
+          balance: "1000000000000000000000000", // 1000000 ETH
+        },
+        {
+          privateKey: "0xdbda1821b80551c9d65939329250298aa3472ba22feea921c0cf5d620ea67b97",
+          balance: "1000000000000000000000000", // 1000000 ETH
+        },
+        {
+          privateKey: "0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6",
+          balance: "1000000000000000000000000", // 1000000 ETH
+        },
+        {
+          privateKey: "0xf214f2b2cd398c806f84e317254e0f0b801d0643303237d97a22a48e01628897",
+          balance: "1000000000000000000000000", // 1000000 ETH
+        },
+      ],
     },
     localnet1: {
       url: 'http://127.0.0.1:8545',
@@ -18,7 +64,7 @@ const config: HardhatUserConfig = {
     localnet2: {
       url: 'http://127.0.0.1:8546',
       chainId: 1338
-    }
+    },
   }
 };
 
